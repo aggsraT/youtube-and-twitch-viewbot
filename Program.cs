b5904da7543144d78b552bf1cc1030ab87274a24
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:05:57 +0000

    Merge code to follow object-oriented principles

--- Program.cs
+++ Program.cs
@@ -400,4 +400,4 @@ namespace Youtube_Viewers
         private static Random random = new Random();
         public static string GetCPN()
         {
-            const string chars = "ABCDEFGHIJK
+            const string chars = "ABCDEFG
