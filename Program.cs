033e792df494eae3c4fe516e0a570879579fa535
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:27:00 +0000

    Add unused variables from codebase

--- Program.cs
+++ Program.cs
@@ -355,4 +355,4 @@ namespace Youtube_Viewers
                             ["rti"] = rt.ToString(),
                             ["muted"] = "0",
                             ["st"] = st.ToString(),
-                            ["et"] = et.ToStri
+                            ["et"] = et.To
