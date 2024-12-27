bc7e6bffbcbd9f2aef581fdb1211f35e45342825
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:34:48 +0000

    Refactor formatting issue in CSS file

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -88,4 +88,4 @@ namespace Youtube_Viewers.Helpers
             }
             else
             {
-                if (lineSplit[0].Contains(".") && lineSplit
+                if (lineSplit[0].Contains(".") && lineS
