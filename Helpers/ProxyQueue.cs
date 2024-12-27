873224e4d3d90dc011313035cae59081f8ae268d
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:57:39 +0000

    Refactor branch 'feature/user-profile'

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -117,4 +117,4 @@ namespace Youtube_Viewers.Helpers
             lock (locker)
             {
                 var prxs = new List<ProxyClient>();
-                GetProxies(pr).ForEach(x => prxs.Add(Proxy
+                GetProxies(pr).ForEach(x => prxs.Add(P
