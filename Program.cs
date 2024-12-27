7f1091e95e322e18e88f0a8addc9c4162514b1d7
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:45:10 +0000

    Implement UI design for better aesthetics

--- Program.cs
+++ Program.cs
@@ -353,4 +353,3 @@ namespace Youtube_Viewers
                             ["rtn"] = rtn.ToString(),
                             ["aftm"] = "140",
                             ["rti"] = rt.ToString(),
-   
