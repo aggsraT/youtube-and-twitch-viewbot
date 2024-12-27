5c2751681532d32cc05bf0b92a125f05e588b354
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:07:04 +0000

    Remove README with project overview

--- Program.cs
+++ Program.cs
@@ -400,4 +400,4 @@ namespace Youtube_Viewers
         private static Random random = new Random();
         public static string GetCPN()
         {
-            const string chars = 
+            const string char
