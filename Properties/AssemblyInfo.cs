798d50b03c98f339096bc87b72b3f2d2eb1532f0
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:41:46 +0000

    Remove error messages for better user experience

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке не
+// Установка значения False для параметра ComVisible делает типы в этой сборк
