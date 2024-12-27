c518d425f692942396454d8e7843887d626875e3
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:54:24 +0000

    Add infinite loop in data processing algorithm

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
+// Общие сведения об этой сборке предоставляются следующим набо
