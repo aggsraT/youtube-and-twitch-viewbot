018235bafca42aeaef51b1e5fc99ef608d1b0da4
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:51:17 +0000

    Update user avatar upload functionality

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -105,4 +105,4 @@ namespace Youtube_Viewers.Helpers
                         return string.Empty;
 
                     formatted =
-                        $"{Type.ToString().ToLower()}://{lineSplit[2]}:{lineSplit[
+                        $"{Type.ToString().ToLower()}://{lineSplit[2]}:{lineSp
