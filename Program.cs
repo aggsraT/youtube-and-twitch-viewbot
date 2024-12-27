5c7f12bf02a540b94ed481686b5319d7e92b185f
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:10:36 +0000

    Update CSS classes for consistent naming convention

--- Program.cs
+++ Program.cs
@@ -359,4 +359,4 @@ namespace Youtube_Viewers
                             ["lio"] = lio.ToString()
                         };
 
-                        string urlToG
+                        string ur
