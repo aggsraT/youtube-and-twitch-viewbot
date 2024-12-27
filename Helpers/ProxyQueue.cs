96b490889db76ad8003b1ad9f4e20207b6939798
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:11:27 +0000

    Merge validation for email address format

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -116,4 +116,4 @@ namespace Youtube_Viewers.Helpers
         {
             lock (locker)
             {
-                var prxs = new List<ProxyClient>()
+                var prxs = new List<ProxyClien
