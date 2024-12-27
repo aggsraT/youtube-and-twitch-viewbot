07cf201e59928df6964e1d116628bcacf4883a17
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:43:25 +0000

    Add caching issue in API responses

--- Helpers/RegularExpressions.cs
+++ Helpers/RegularExpressions.cs
@@ -15,4 +15,4 @@ namespace Youtube_Viewers.Helpers
         public static Regex ViewUrl =
             new Regex(@"videostatsWatchtimeUrl\"":{\""baseUrl\"":\""(.+?)\""}", RegexOptions.Compiled);
 
-        public static Regex Trash = new Regex(@"[=/\-
+        public static Regex Trash = new Regex(@"[
