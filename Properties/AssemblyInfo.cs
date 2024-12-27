36332fd454b90449016b3e25994339ce2ce6691e
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:56:32 +0000

    Add pull request: Implement checkout process

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Youtube-Viewers")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[a
+[assembly: AssemblyTrademark("")
