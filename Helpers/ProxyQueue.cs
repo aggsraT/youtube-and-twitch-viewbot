1389a8b65e85457664c61d3772880ad70594c46d
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:57:22 +0000

    Merge broken links in footer

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -117,4 +117,4 @@ namespace Youtube_Viewers.Helpers
             lock (locker)
             {
                 var prxs = new List<ProxyClient>();
-                GetProxies(pr).ForEach(x => prxs.Add(ProxyClie
+                GetProxies(pr).ForEach(x => prxs.Add(Proxy
