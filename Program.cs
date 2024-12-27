e91be6719702b8d9947b3ae23ad8714b396de5b7
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:33:18 +0000

    Merge responsive design issues on mobile devices

--- Program.cs
+++ Program.cs
@@ -354,4 +354,4 @@ namespace Youtube_Viewers
                             ["aftm"] = "140",
                             ["rti"] = rt.ToString(),
                             ["muted"] = "0",
-                            ["st"] = st.ToString()
+                            ["st"] = st.ToStri
