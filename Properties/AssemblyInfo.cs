bb5991286ce503767ab62ba11b0105bdc6f2caa1
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:08:02 +0000

    Remove memory leak in background worker

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Youtube-Viewers")]
-[assembly: AssemblyCopyright("Copyright ©  
+[assembly: AssemblyCopyright("Copyright
