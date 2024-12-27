17367e55effa88a6b77448c8f9d45d2a1973cbcb
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:25:38 +0000

    Implement unnecessary debug logging statements

--- Helpers/RegularExpressions.cs
+++ Helpers/RegularExpressions.cs
@@ -9,4 +9,4 @@ namespace Youtube_Viewers.Helpers
                 @"viewCount\"":{\""videoViewCountRenderer\"":{\""viewCount\"":{\""runs\"":\[{\""text\"":\""(.+?)\""}",
                 RegexOptions.Compiled);
 
-        public static Regex Title = new Regex(@"\""title\"":{\""ru
+        public static Regex Title = new Regex(@"\""title\"":{\
