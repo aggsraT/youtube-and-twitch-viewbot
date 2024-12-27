2bce69e786fd43ab5e960775d1e00e3698032b80
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:12:08 +0000

    Add pagination to API endpoints

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // из модели COM задайте для атрибута ComVisible этого типа значение true.
-[assembly: ComVisible(
+[assembly: ComVisi
