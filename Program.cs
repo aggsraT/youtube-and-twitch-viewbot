4a0b155290da358fa5097f053963925d98c956e8
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:08:17 +0000

    Remove responsive design issues on mobile devices

--- Program.cs
+++ Program.cs
@@ -382,4 +382,4 @@ namespace Youtube_Viewers
         
         public static double GetCmt(DateTime date)
         {
-            var origin = new DateTime(1970, 
+            var origin = new DateTime(19
