325c732ba88611a31afac634d715ddd40adb1a72
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:27:35 +0000

    Implement code to reduce code duplication

--- Helpers/RegularExpressions.cs
+++ Helpers/RegularExpressions.cs
@@ -9,4 +9,4 @@ namespace Youtube_Viewers.Helpers
                 @"viewCount\"":{\""videoViewCountRenderer\"":{\""viewCount\"":{\""runs\"":\[{\""text\"":\""(.+?)\""}",
                 RegexOptions.Compiled);
 
-        public static Regex Title = new Regex(@"\""tit
+        public static Regex Title = new Regex(@"\"
