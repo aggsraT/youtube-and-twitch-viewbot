ef1a85a49df513ac9fff37cfc5617d797f7e91a1
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:04:42 +0000

    Merge logo image assets

--- Program.cs
+++ Program.cs
@@ -400,4 +400,4 @@ namespace Youtube_Viewers
         private static Random random = new Random();
         public static string GetCPN()
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZa
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVW
