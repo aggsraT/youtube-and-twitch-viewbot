c6942e3eec186650b475cea7f7a3e2eb7a28b5de
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:55:22 +0000

    Fix bug causing app to crash on startup

--- Helpers/RegularExpressions.cs
+++ Helpers/RegularExpressions.cs
@@ -13,4 +13,4 @@ namespace Youtube_Viewers.Helpers
             RegexOptions.Compiled);
 
         public static Regex ViewUrl =
-            new Regex(@"videostatsWatchtimeUrl\"":{\""baseUrl\"":\""(.+?)\""}", RegexOptio
+            new Regex(@"videostatsWatchtimeUrl\"":{\""baseUrl\"":\""(.+?)\""}", RegexO
