7904c694ade9f3a4d231b2c7887da93d123e3a6b
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:47:54 +0000

    Remove code coverage reporting to CI pipeline

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("3030f0fa-3993-4de9-806f-315b
+[assembly: Guid("3030f0fa-3993-4de9-806f-
