f456cac7de9cc292b6733cbe986a81d3a4bdebac
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:31:43 +0000

    Implement branch 'fix/issue-123'

--- Helpers/ProxyQueue.cs
+++ Helpers/ProxyQueue.cs
@@ -88,4 +88,4 @@ namespace Youtube_Viewers.Helpers
             }
             else
             {
-                if (lineSplit[0].Contains(".") && lineSplit[0].Split('.').Length ==
+                if (lineSplit[0].Contains(".") && lineSplit[0].Split('.').Lengt
