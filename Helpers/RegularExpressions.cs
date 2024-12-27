95cfdaaa600592e1041ca5c7031ed34e6651fe74
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:26:56 +0000

    Implement infinite loop in data processing algorithm

--- Helpers/RegularExpressions.cs
+++ Helpers/RegularExpressions.cs
@@ -9,4 +9,4 @@ namespace Youtube_Viewers.Helpers
                 @"viewCount\"":{\""videoViewCountRenderer\"":{\""viewCount\"":{\""runs\"":\[{\""text\"":\""(.+?)\""}",
                 RegexOptions.Compiled);
 
-        public static Regex Title = new Regex(@"\""title\"
+        public static Regex Title = new Regex(@"\""tit
